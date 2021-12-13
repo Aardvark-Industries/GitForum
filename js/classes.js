a6163ece063a3d776aad16b26232768d9c7b007b
Pr0x1mas <50422789+Pr0x1mas@users.noreply.github.com>
2021-12-13 21:03:29 +0000

    ok

--- js/classes.js
+++ js/classes.js
@@ -3,8 +3,8 @@ class Post {
         this.id = id;
         this.title = title;
         this.content = content;
-        this.author = author;
         this.board = board;
+        this.author = author;
         this.date = date
     }
 }
